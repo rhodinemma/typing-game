13431f4a3e05c08827d85e7b686fb51dd9c9a7e8
rhodinemma <rhodinemma10@gmail.com>
2023-03-02 11:33:39 +0300

    feat: styling by comparison

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,4 +13,12 @@ export class AppComponent {
   onInput(value: string) {
     this.enteredText = value;
   }
+
+  compare(randomLetter: string, enteredLetter: string) {
+    if (!enteredLetter) {
+      return 'pending';
+    }
+
+    return randomLetter === enteredLetter ? 'correct' : 'incorrect';
+  }
 }
